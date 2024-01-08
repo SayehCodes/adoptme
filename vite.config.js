9c79e2a80c4fe99f17f81a75dc26881ef658d1fb
sayehcodes <sayeh.ghaderi9@outlook.com>
2024-01-08 15:17:58 +0100

    Added some script for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  build: {
+    outDir: "dist", // Specify the output directory
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
 });
