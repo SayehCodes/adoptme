9807c0b93ed842c4df589d0d107a44152994fe9e
sayehcodes <sayeh.ghaderi9@outlook.com>
2024-01-10 15:48:00 +0100

    Added some script for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,5 @@ export default defineConfig({
   root: "src",
   build: {
     outDir: "../dist", // Specify the output directory
-<<<<<<< HEAD
-=======
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      // include: [/linked-dep/, /node_modules/],
-    },
->>>>>>> 1ba00b86a66c23679977b4f155086ca8685b4e20
   },
 });
